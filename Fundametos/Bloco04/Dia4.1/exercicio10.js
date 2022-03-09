d3617dbd0278ef2a04a777d1dfa43f7f8911f5bb
Sophia <sophiasiqueiram@gmail.com>
2022-03-09 03:03:16 -0300

    Dia 4.2

--- Fundametos/Bloco04/Dia4.1/exercicio10.js
+++ Fundametos/Bloco04/Dia4.1/exercicio10.js
@@ -1,5 +1,5 @@
 // Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
 
-const custo;
-const venda;
+let custo;
+let venda;
 
--- Fundametos/Bloco04/Dia4.1/exercicio4.js
+++ Fundametos/Bloco04/Dia4.1/exercicio4.js
@@ -2,7 +2,7 @@
 
 const num = 5
 
-if (num > 0){
+if (num >= 0){
   console.log("positive");
 }
 else if (num < 0){
