b7530288b87c7a624d164368504fd24aa565fb55
Sophia <sophiasiqueiram@gmail.com>
2022-03-14 19:01:05 -0300

    exercicio 5 dia 4.2

--- Fundametos/Bloco04/Dia4.2/exercicio5.js
+++ Fundametos/Bloco04/Dia4.2/exercicio5.js
@@ -2,6 +2,13 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (i = 0; i < numbers.length; i += 1){
-  
-}
+let maior = 0;// criando a variavel maior com o valor 0
+
+// for (i = 0; i < numbers.length; i += 1){ // i tem o valor de 0; enquanto i for menor que a quantidade de array; i anda de um em um no array
+    for(let i of numbers){ // pega i de numbers (ou seja, pega cada casa de numbers)
+      if(i > maior){ // se i for maior que a variavel "maior" (que é zero)
+          maior = i; // então a variavel maior recebe o valor do número
+      }
+  }
+// }
+console.log(maior) // e quando acabar o loop, o maior numero é impresso
