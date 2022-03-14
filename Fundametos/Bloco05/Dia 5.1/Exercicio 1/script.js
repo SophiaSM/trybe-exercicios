0898da648872b4d64c53df9a54557c2832451f6e
Sophia <sophiasiqueiram@gmail.com>
2022-03-14 19:02:29 -0300

    exercicio 1 dia 5.1

---
+++ Fundametos/Bloco05/Dia 5.1/Exercicio 1/script.js
@@ -0,0 +1,11 @@
+document.getElementById("header-container").style.backgroundColor = "Green"
+
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "salmon"
+document.getElementsByClassName("margem1")[0].style.backgroundColor = "purple"
+document.getElementsByClassName("margem1")[1].style.backgroundColor = "purple"
+
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "yellow"
+document.getElementsByClassName("margem2")[0].style.backgroundColor = "black"
+document.getElementsByClassName("margem2")[1].style.backgroundColor = "black"
+
+document.getElementById("footer-container").style.backgroundColor = "darkgreen"
