0c4391344b7db010192bd85d3486453b2f5cffae
Sophia <sophiasiqueiram@gmail.com>
2022-03-14 19:00:52 -0300

    Exercicio 3 dia 4.2

--- Fundametos/Bloco04/Dia4.2/exercicio3.js
+++ Fundametos/Bloco04/Dia4.2/exercicio3.js
@@ -4,7 +4,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let sum = 0 // cria a variavel soma e da o valor de zero
-for (i = 0; i < numbers.length; i += 1){ // i é igual a zero; enquanto i for menor que o tamanho do array, retorna i refazendo o loop somando mais um
+for (i = 0; i < numbers.length; i += 1){ // i é igual a zero; enquanto i for menor que o tamanho do array, retorna i refazendo caminhando de um em um 
   sum += numbers[i]; // soma é o index de numeros (que é o resultado do loop)
 }
 let media = (sum / 2); //aqui ele da o valor de soma dividido por dois como o resultado de media
